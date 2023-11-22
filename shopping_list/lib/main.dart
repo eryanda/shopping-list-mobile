6fa3867560e2e88336ad93bc6e9875a51589fc31
eryanda <eryanduty@gmail.com>
2023-11-22 15:33:19 +0700

    tutorial 9 selesai

--- shopping_list/lib/main.dart
+++ shopping_list/lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:shopping_list/screens/menu.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
-import 'screens/login.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_list/screens/login.dart';
+import 'package:shopping_list/screens/menu.dart';
 
 void main() {
   runApp(const MyApp());
